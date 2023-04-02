2584cdf5480d8fd5346d0cf303bbdce8437f2019
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-04-02 10:52:17 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors"
 import path from "path"
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 connectDB();
-
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //middlewares
